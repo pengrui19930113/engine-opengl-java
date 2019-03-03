52053854f362c036a61797571e12a8ab662a00ed
pengrui <362573456@qq.com>
2019-03-03 09:14:23 +0800

    just add the billboard effect interface

---
+++ src/main/java/pengrui/javagl/abstraction/effect/Billboard.java
@@ -0,0 +1,16 @@
+package pengrui.javagl.abstraction.effect;
+
+/**
+ * 
+ * alway face to the camera
+ * 
+ * 
+ * 广告牌技术
+ * 渲染效果的时候总是面对着观察点
+ * 
+ * @author Administrator
+ *
+ */
+public interface Billboard {
+
+}
